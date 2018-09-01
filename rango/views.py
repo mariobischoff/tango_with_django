4ad0360ad5305710054c27c299793856f3428c77
Mário Bischoff <mariobischoffneto@hotmail.com>
2018-09-01 16:07:26 -0300

    add template and models

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'my_name': "Mário Bischoff"}
+    return render(request, 'rango/about.html', context=context_dict)
